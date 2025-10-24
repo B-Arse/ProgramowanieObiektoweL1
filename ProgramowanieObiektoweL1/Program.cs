f13a9eea0e01a4da64784cc2011b744c8abdc132
B-Arse <arsenbojcuk20@gmail.com>
2025-10-24 14:33:06 +0200

    Zmieniono tekst konsoli w gałęzi NowaGalaz

--- ProgramowanieObiektoweL1/Program.cs
+++ ProgramowanieObiektoweL1/Program.cs
@@ -3,6 +3,6 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello from the new branch!");
     }
 }
