d9a2bca5b5b5a4d5ce68dfed6b07e5c397fdc8d5
B-Arse <arsenbojcuk20@gmail.com>
2025-10-27 14:27:21 +0100

    Laboratorium 2 zadania (1–7)

---
+++ Lab2/Program.cs
@@ -0,0 +1,67 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Zwierze zwierze = new Zwierze("");
+        Pies pies = new Pies("Oddy");
+        Kot kot = new Kot("Marik");
+        Waz waz = new Waz("Nagini");
+
+   
+        powiedz_cos(zwierze);
+        powiedz_cos(pies);
+        powiedz_cos(kot);
+        powiedz_cos(waz);
+}
+    static void powiedz_cos(Zwierze zwierze)
+    {
+        zwierze.daj_glos();
+        Console.WriteLine($"Typ obiektu: {zwierze.GetType().Name}");
+    }
+}
+class Zwierze
+{
+    protected string nazwa;
+    public Zwierze(string nazwa)
+    {
+        this.nazwa = nazwa;
+    }
+    public virtual void daj_glos()
+    {
+        Console.WriteLine("...");
+    }
+  
+}
+class Pies: Zwierze
+{
+    public Pies(string nazwa) : base(nazwa)
+    {
+
+    }
+    public override void daj_glos()
+    {
+        Console.WriteLine($"{nazwa} robi woof woof!");
+    }
+}
+class Kot: Zwierze
+{
+    public Kot(string nazwa) : base(nazwa)
+    {
+
+    }
+    public override void daj_glos()
+    {
+        Console.WriteLine($"{nazwa} robi miau miau!");
+    }
+}
+class Waz: Zwierze
+{
+    public Waz(string nazwa) : base(nazwa)
+    {
+
+    }
+    public override void daj_glos()
+    {
+        Console.WriteLine($"{nazwa} robi ssssssss!");
+    }
+}
